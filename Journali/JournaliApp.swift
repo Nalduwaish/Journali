f801d844d42323844a9c6c697346618f044edfab
Nalduwaish <Najlaldweesh@gmail.com>
2025-10-19 10:07:49 +0300

    Initial Commit

---
+++ Journali/JournaliApp.swift
@@ -0,0 +1,17 @@
+//
+//  JournaliApp.swift
+//  Journali
+//
+//  Created by Najla on 27/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct JournaliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
